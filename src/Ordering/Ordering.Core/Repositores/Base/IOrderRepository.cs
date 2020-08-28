c546ed5da2df8b79d3ddcb7abcea5f003599de4d
Giardzz <giardz@icloud.com>
2020-08-28 20:44:32 +0200

    Added ordering micro service

---
+++ src/Ordering/Ordering.Core/Repositores/Base/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Core.Repositores.Base
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrderByUserName(string userName);
+    }
+}
